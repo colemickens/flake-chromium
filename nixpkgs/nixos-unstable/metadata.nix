2391b15fe01e4816e09ebb3d47547ac01978b3c7
Cole Mickens <cole.mickens@gmail.com>
2020-03-02 10:00:27 +0000

    wip

--- nixpkgs/nixos-unstable/metadata.nix
+++ nixpkgs/nixos-unstable/metadata.nix
@@ -2,7 +2,7 @@
   repo_git = "https://github.com/nixos/nixpkgs";
   branch = "nixos-unstable";
   url = "https://github.com/nixos/nixpkgs/archive/nixos-unstable.tar.gz";
-  rev = "a21c2fa3ea2b88e698db6fc151d9c7259ae14d96";
-  sha256 = "1z3kxlbz6bqx1dlagcazg04vhk67r8byihzf959c3m0laf2a1w7y";
-  revdate = "2020-01-31 10:48:37 +0100";
+  rev = "42f0be81ae05a8fe6d6e8e7f1c28652e7746e046";
+  sha256 = "1rxb5kmghkzazqcv4d8yczdiv2srs4r7apx4idc276lcikm0hdmf";
+  revdate = "2020-03-01 07:21:40 -0300";
 }
